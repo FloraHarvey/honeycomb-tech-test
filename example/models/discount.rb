35717b825616a2a90d3a4502fc32b064e9696d54
Flora Harvey <fharvey555@gmail.com>
2017-05-10 10:02:39 +0100

    Add methods to define conditions of discount and check whether it applies

--- example/models/discount.rb
+++ example/models/discount.rb
@@ -1,21 +1,43 @@
 class Discount
 
-  def initialize(type, discount)
+  def initialize(type, discount, item = :none)
     @type = type
     @discount = discount
+    @item = item
   end
 
-  def calculate(total_cost)
-    total_cost / @discount
+  def calculate(total_cost, items)
+    if @type === :percent_off
+      total_cost / @discount
+    elsif @type === :delivery_price_reduction
+      calculate_delivery_reduction_discount(items)
+    end
   end
 
-  def define_condition(property, min_amount)
-    @property = property
-    @min_amount = min_amount
+  def define_min_spend(min_spend)
+    @min_spend = min_spend
   end
 
-  def applies?(total_cost)
-    @property === :total && total_cost > @min_amount
+  def define_min_deliveries(min_deliveries)
+    @min_deliveries = min_deliveries
+  end
+
+  def applies?(total_cost, items)
+    !@min_spend && !@min_deliveries ||
+    !!@min_spend && total_cost >= @min_spend ||
+    !!@min_deliveries && applicable_deliveries(items).count >= @min_deliveries
+  end
+
+  def applicable_deliveries(items)
+    items.select { |item|
+      item[1].name === @item
+    }
+  end
+
+  def calculate_delivery_reduction_discount(items)
+    deliveries = applicable_deliveries(items)
+    price_difference = deliveries.first[1].price - @discount
+    deliveries.count * price_difference
   end
 
 end
--- example/spec/discount_spec.rb
+++ example/spec/discount_spec.rb
@@ -1,24 +1,50 @@
 require './models/discount'
+require './models/delivery'
 
 describe Discount do
 
+  let(:standard_delivery) { Delivery.new(:standard, 10) }
+  let(:express_delivery) { Delivery.new(:express, 20) }
+  let(:example_items1) { [['Sky', standard_delivery], ['Disney', standard_delivery]] }
+  let(:example_items2) { [['Sky', express_delivery], ['Disney', express_delivery]] }
+  let(:example_items3) { [['Sky', standard_delivery]] }
+
+
   describe 'defining a new discount' do
 
-    context 'percent_off' do
-
-      it 'receives parameters defining details of the discount' do
-        discount = Discount.new(:percent_off, 10)
-        expect(discount.calculate(20)).to eq(2)
-      end
-
-      it 'can have conditions set for which it applies' do
-        discount = Discount.new(:percent_off, 10)
-        discount.define_condition(:total, 30)
-        expect(discount.applies?(35)).to eq(true)
-        expect(discount.applies?(20)).to eq(false)
-      end
+    it 'receives parameters defining details of percent-off discount' do
+      discount = Discount.new(:percent_off, 10)
+      expect(discount.calculate(20, example_items1)).to eq(2)
     end
 
+    it 'receives parameters defining details of the delivery price reduction discount' do
+      discount = Discount.new(:delivery_price_reduction, 15, :express)
+      expect(discount.calculate(40, example_items2)).to eq(10)
+    end
+
+    describe 'setting conditions for discount' do
+
+      context 'percent_off' do
+
+        it 'can have conditions set for which it applies' do
+          discount = Discount.new(:percent_off, 10)
+          discount.define_min_spend(30)
+          expect(discount.applies?(20, example_items1)).to eq(false)
+          expect(discount.applies?(40, example_items1)).to eq(true)
+        end
+      end
+
+      context 'certain number of items' do
+
+        it 'receives parameters defining details of the discount' do
+          discount = Discount.new(:delivery_price_reduction, 15, :express)
+          discount.define_min_deliveries(2)
+          expect(discount.applies?(30, example_items1)).to eq(false)
+          expect(discount.applies?(30, example_items2)).to eq(true)
+          expect(discount.applies?(30, example_items3)).to eq(false)
+        end
+      end
+    end
 
   end
 
